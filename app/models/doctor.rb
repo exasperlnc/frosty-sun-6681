1be00f2cdb2c0dd8dfc3ce94697bf7863d3f5e5e
Logan Cole <emtlogancole@gmail.com>
2023-04-24 13:02:29 -0500

    comment out unfinished method

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,4 +6,8 @@ class Doctor < ApplicationRecord
   def find_doctor_patient_by_patient(patient_id)
     doctor_patients.where(patient_id: patient_id).pluck(:id)
   end
+
+  # def self.doctor_patient_count
+  #   select("doctors.*, sum(doctor_patients) AS patient_count").joins(:doctor_patien).group(:id)
+  # end
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -16,5 +16,14 @@ RSpec.describe Doctor do
       expect(@doctor_1.find_doctor_patient_by_patient(@patient_2.id)).to eq([@doctorpatient_2.id])
       expect(@doctor_2.find_doctor_patient_by_patient(@patient_3.id)).to eq([@doctorpatient_4.id])
     end
+  
   end
+  # describe "#class methods" do
+  #   it 'lists number of patients' do
+  #     require 'pry'; binding.pry
+  #     expect(@doctor_1.doctor_patient_count.patient_count).to eq(2)
+  #     expect(@doctor_2.doctor_patient_count.patient_count).to eq(2)
+  #     expect(@doctor_3.doctor_patient_count.patient_count).to eq(0)
+  #   end
+  # end
 end
